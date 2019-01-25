16d311e2d33b5d71d09099888ef4e33debcd28f3
j0hnb00k <johnaertsbe@gmail.com>
2019-01-25 21:51:51 +0100

    angular/cli

--- apitest/src/app/app-routing.module.ts
+++ apitest/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
 
 const routes: Routes = [];
 
--- apitest/src/system.js
+++
@@ -1,3 +0,0 @@
-map: {
-    '@ng-bootstrap/ng-bootstrap': 'node_modules/@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.js',
-  }
