b83bb2ec0fbfb37e9f0da0b5adb12cae36faedbf
j0hnb00k <johnaertsbe@gmail.com>
2019-01-25 23:19:16 +0100

    test

--- apitest/src/app/app-routing.module.ts
+++ apitest/src/app/app-routing.module.ts
@@ -5,9 +5,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), NgbModule],
+
+  imports: [
+    RouterModule.forRoot(routes), 
+    NgbModule
+  ],
   exports: [RouterModule]
+
 })
+
 export class AppRoutingModule { }
 
 
