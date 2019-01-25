9b96b48ab7d36df8d3ec8a7cf2356d37869a4c84
mliuej <geoffrey.deneyer@gmail.com>
2019-01-26 00:34:05 +0100

    premiere page

--- apitest/src/app/app.module.ts
+++ apitest/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProfilComponent } from './profil/profil.component';
+import { ApirankComponent } from './apirank/apirank.component';
+import { CompetitionComponent } from './competition/competition.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProfilComponent,
+    ApirankComponent,
+    CompetitionComponent
   ],
   imports: [
     BrowserModule,
