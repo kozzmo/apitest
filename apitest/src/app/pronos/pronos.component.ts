afbf86e92638dc31502dcc5667c72c876354a1bc
j0hnb00k <johnaertsbe@gmail.com>
2019-01-25 20:38:00 +0100

    new files

---
+++ apitest/src/app/pronos/pronos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pronos',
+  templateUrl: './pronos.component.html',
+  styleUrls: ['./pronos.component.scss']
+})
+export class PronosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
